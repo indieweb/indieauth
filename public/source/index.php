3c72547096ba7e086862aa5c9caf6bb76e1a0fc9
Aaron Parecki <aaron@parecki.com>
2017-12-04 10:57:22 -0800

    update editor's draft

--- public/source/index.php
+++ public/source/index.php
@@ -8,7 +8,7 @@
     <script class='remove'>
       var respecConfig = {
           useExperimentalStyles: true,
-          publishDate: "2017-11-28",
+          publishDate: "2017-12-04",
           specStatus: "NOTE",
           shortName:  "indieauth",
           edDraftURI: "https://indieauth.net/spec/",
