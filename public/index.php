3c49d00ed7a2357d027580f8a88b9e71cdb90389
Aaron Parecki <aaron@parecki.com>
2018-06-05 09:51:24 -0700

    update home page

--- public/index.php
+++ public/index.php
@@ -26,7 +26,11 @@
 
   <p>The latest version of the IndieAuth spec is available at:</p>
 
-  <p><a href="/spec/">indieauth.net/spec</a></p>
+  <p><a href="https://indieauth.spec.indieweb.org/">indieauth.spec.indieweb.org</a></p>
+
+  <p>The January 2018 published version is also available at:</p>
+
+  <p><a href="https://www.w3.org/TR/indieauth/">w3.org/TR/indieauth</a></p>
 </section>
 
 <section class="hang-left border-top">
@@ -35,7 +39,7 @@
   <h2>Logging in with IndieAuth</h2>
 
   <p>You can use IndieAuth to have your users authenticate with their own URL. Logging in to an app with IndieAuth tells the app who has logged in, where the user ID returned is a URL controlled by the user.</p>
-  <p><a href="https://indieauth.net/spec/#authentication">Logging in with IndieAuth</a></p>
+  <p><a href="https://indieauth.spec.indieweb.org/#authentication">Logging in with IndieAuth</a></p>
 </section>
 
 <section class="border-top">
@@ -43,7 +47,7 @@
 
   <p>If you're building an application that wants to access or modify a user's data, you'll need an OAuth 2.0 access token to use in API requests.</p>
   <p>You can use IndieAuth to obtain an access token from the user's own token endpoint, while identifying them in the process.</p>
-  <p><a href="https://indieauth.net/spec/#authorization">Obtaining an Access Token</a></p>
+  <p><a href="https://indieauth.spec.indieweb.org/#authorization">Obtaining an Access Token</a></p>
 </section>
 
 <section class="border-top">
@@ -60,6 +64,8 @@
   <ul>
     <li><a href="https://github.com/Inklings-io/selfauth">selfauth</a></li>
     <li><a href="https://github.com/cweiske/indieauth-openid">indieauth-openid</a> - proxies IndieAuth requests to your own OpenID provider</li>
+    <li><a href="https://github.com/barryf/acquiescence">Acquiescence</a> - an IndieAuth server written in Ruby which uses GitHub to authenticate you</li>
+    <li><a href="https://indieweb.org/Wordpress_IndieAuth_Plugin">WordPress IndieAuth Plugin</a> - adds an IndieAuth endpoint to your WordPress site</li>
   </ul>
 
   <h3>Software with a Built-In IndieAuth Provider</h3>
@@ -68,13 +74,24 @@
   </ul>
 </section>
 
+
+<section class="border-top">
+  <h2>Frequently Asked Questions</h2>
+
+  <h3 id="differences-from-openid-connect">How is IndieAuth different from OpenID Connect?</h3>
+  <p>See <a href="https://indieweb.org/How_is_IndieAuth_different_from_OpenID_Connect">indieweb.org/How_is_IndieAuth_different_from_OpenID_Connect</a></p>
+
+</section>
+
+
 </article>
 
 <footer>
   <section>
     <h3>More Resources</h3>
     <ul>
-      <li><a href="https://indieweb.org/Category:IndieAuth">More about IndieAuth on indieweb.org</a></li>
+      <li><a href="https://indieweb.org/IndieAuth">IndieAuth on indieweb.org</a></li>
+      <li><a href="https://indieweb.org/Category:IndieAuth">IndieAuth details and tutorials</a></li>
       <li><a href="https://oauth2simplified.com/">OAuth 2.0 Simplified</a></li>
       <li><a href="https://micropub.net/">Micropub</a></li>
     </ul>
